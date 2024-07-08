a56533dc1d8e9bd6c5dfc3d4c368cf43f88cc033
백승주 <tmdwn3431@naver.com>
2024-07-08 23:58:56 +0900

    Test1 - json, 싱글톤
    ID 받아오는 부분 수정해야 함

--- Assets/3.Script/Json/Recipe_DataManager.cs
+++ Assets/3.Script/Json/Recipe_DataManager.cs
@@ -2,21 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
+using System.IO;
 
 
-class Recipe_DB
+public class Recipe_DataManager : MonoBehaviour
 {
-    public int stage;
-    public string recipe;
-    public string ingredient;
 
-}
-public class Recipe_DataManager
-{
-    Recipe_DB recipe = new Recipe_DB();
+    private List<Recipe> recipes;
+    public static Recipe_DataManager instance;
 
-    private void Start()
+   public static Recipe_DataManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<Recipe_DataManager>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject("Recipe_DataManager");
+                    instance = obj.AddComponent<Recipe_DataManager>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
     {
         
     }
+
+    private void LoadRecipeFromJson()
+    {
+        TextAsset jsonFile = Resources.Load<TextAsset>("Recipe_JDB");
+        recipes = JsonConvert.DeserializeObject<List<Recipe>>(jsonFile.text);
+    }
+
+    //public Recipe_DataManager GetRecipe_ID(int id)
+    //{
+    //    return recipes.Find(recipe => recipe.id == id);
+    //}
 }
+
