44e1f159d2e52ff375f0dee7d94a806f7861186a
최정문 <>
2024-07-10 14:13:19 +0900

    [다시연결test]

--- Assets/3.Script/ETC/EmissionController.cs
+++ Assets/3.Script/ETC/EmissionController.cs
@@ -4,42 +4,61 @@ using UnityEngine;
 
 public class EmissionController : MonoBehaviour
 {
+    [SerializeField] private float interactRadius = 1f;
+    private bool isSelect = false;
+    private Queue<GameObject> pickQue;
+
     private Material originalMaterial;
     [SerializeField] private Material instanceMaterial;
 
     private void Awake()
     {
+        pickQue = new Queue<GameObject>();
+
         instanceMaterial.EnableKeyword("_EMISSION");
         instanceMaterial.SetColor("_EmissionColor", new Color(60, 60, 60)/* * Mathf.LinearToGammaSpace(2.0f)*/);
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if(collision.transform.CompareTag("TEST"))
+        if (collision.gameObject.CompareTag("TEST"))
         {
-            // 충돌한 오브젝트의 렌더러 가져오기
-            Renderer renderer = collision.gameObject.GetComponent<Renderer>();
-
-            if (renderer != null)
+            if(pickQue.Count.Equals(0))
             {
-                // 원래의 머테리얼 저장
-                originalMaterial = renderer.material;
-
-                // 복사한 머테리얼로 변경
-                renderer.material = instanceMaterial;
-                Debug.Log("충돌");
+               pickQue.Enqueue(collision.gameObject);
             }
         }
+
+        //if (collision.transform.CompareTag("TEST"))
+        //{
+        //    // 충돌한 오브젝트의 렌더러 가져오기
+        //    Renderer renderer = collision.gameObject.GetComponent<Renderer>();
+
+        //    if (renderer != null)
+        //    {
+        //        // 원래의 머테리얼 저장
+        //        originalMaterial = renderer.material;
+
+        //        // 복사한 머테리얼로 변경
+        //        renderer.material = instanceMaterial;
+        //        Debug.Log("충돌");
+        //    }
+        //}
     }
 
-    void OnCollisionExit(Collision collision)
+    private void OnCollisionExit(Collision collision)
     {
-        // 충돌이 끝났을 때 원래 머테리얼로 복구
-        Renderer renderer = collision.gameObject.GetComponent<Renderer>();
-
-        if (renderer != null && renderer.material == instanceMaterial)
-        {
-            renderer.material = originalMaterial;
-        }
+        pickQue.Dequeue();
     }
+
+    //void OnCollisionExit(Collision collision)
+    //{
+    //    // 충돌이 끝났을 때 원래 머테리얼로 복구
+    //    Renderer renderer = collision.gameObject.GetComponent<Renderer>();
+
+    //    if (renderer != null && renderer.material == instanceMaterial)
+    //    {
+    //        renderer.material = originalMaterial;
+    //    }
+    //}
 }
