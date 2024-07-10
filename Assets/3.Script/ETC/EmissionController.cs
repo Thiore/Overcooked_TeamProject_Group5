6d681afec8d2f8df1a771d26efb82cf50ad727c0
최정문 <>
2024-07-10 16:49:01 +0900

    [세프 근접시 오브젝트 활성화 표현 나름 완료]

--- Assets/3.Script/ETC/EmissionController.cs
+++ Assets/3.Script/ETC/EmissionController.cs
@@ -4,61 +4,88 @@ using UnityEngine;
 
 public class EmissionController : MonoBehaviour
 {
-    [SerializeField] private float interactRadius = 1f;
-    private bool isSelect = false;
+    private bool isPick = false;
     private Queue<GameObject> pickQue;
 
     private Material originalMaterial;
-    [SerializeField] private Material instanceMaterial;
+    private Material instanceMaterial;
 
     private void Awake()
     {
         pickQue = new Queue<GameObject>();
-
-        instanceMaterial.EnableKeyword("_EMISSION");
-        instanceMaterial.SetColor("_EmissionColor", new Color(60, 60, 60)/* * Mathf.LinearToGammaSpace(2.0f)*/);
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.CompareTag("TEST"))
         {
-            if(pickQue.Count.Equals(0))
+            if(!isPick)
             {
-               pickQue.Enqueue(collision.gameObject);
+                isPick = true;
+                if (pickQue.Count > 0)
+                {
+                    ByeObeject(pickQue.Peek());
+                    pickQue.Clear();
+                    pickQue.Enqueue(collision.gameObject);
+                    PickObject(pickQue.Peek());
+                }
+
+                if (pickQue.Count.Equals(0))
+                {
+                    pickQue.Enqueue(collision.gameObject);
+                    PickObject(pickQue.Peek());
+                }
+
+                Debug.Log("호출");
             }
+
         }
-
-        //if (collision.transform.CompareTag("TEST"))
-        //{
-        //    // 충돌한 오브젝트의 렌더러 가져오기
-        //    Renderer renderer = collision.gameObject.GetComponent<Renderer>();
-
-        //    if (renderer != null)
-        //    {
-        //        // 원래의 머테리얼 저장
-        //        originalMaterial = renderer.material;
-
-        //        // 복사한 머테리얼로 변경
-        //        renderer.material = instanceMaterial;
-        //        Debug.Log("충돌");
-        //    }
-        //}
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        pickQue.Dequeue();
+        if(pickQue.Count > 0)
+        {
+           ByeObeject(pickQue.Peek());
+           pickQue.Clear();
+        }
+
+        isPick = false;
     }
 
-    //void OnCollisionExit(Collision collision)
-    //{
-    //    // 충돌이 끝났을 때 원래 머테리얼로 복구
-    //    Renderer renderer = collision.gameObject.GetComponent<Renderer>();
+    void PickObject(GameObject gameObject)
+    {
 
-    //    if (renderer != null && renderer.material == instanceMaterial)
-    //    {
-    //        renderer.material = originalMaterial;
-    //    }
-    //}
+        // 충돌한 오브젝트의 렌더러 가져오기
+        Renderer renderer = gameObject.gameObject.GetComponent<Renderer>();
+
+        if (renderer != null)
+        {
+            // 원래의 머테리얼 저장
+            originalMaterial = renderer.material;
+
+            // 머테리얼 복사
+            instanceMaterial = new Material(originalMaterial);
+
+            // 복사한 머테리얼로 변경
+            renderer.material = instanceMaterial;
+
+            // Emission 활성화 및 HDR 값 변경
+            instanceMaterial.EnableKeyword("_EMISSION");
+            instanceMaterial.SetColor("_EmissionColor", new Color(0.5f, 0.5f, 0.5f)/* * Mathf.LinearToGammaSpace(2.0f)*/);
+        }
+    }
+
+    void ByeObeject(GameObject gameObject)
+    {
+        // 충돌이 끝났을 때 원래 머테리얼로 복구
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+
+        if (renderer != null)
+        {
+            renderer.material = originalMaterial;
+        }
+    }
 }
+
+
