38a1af37aa86fac1c367acf3fe735df0b3592aba
백승주 <tmdwn3431@naver.com>
2024-07-12 18:02:14 +0900

    [add] Json(stageScore) + MapHelper(오브젝트 메쉬,머테리얼)
    StageRecipeData 메서드 => 각 레시피들 리스트 저장, 레시피들의 재료들 리스트 저장

--- Assets/3.Script/Json/DataManager.cs
+++ Assets/3.Script/Json/DataManager.cs
@@ -14,18 +14,21 @@ public class DataManager : MonoBehaviour
 {
     private static DataManager instance;
     public Dictionary<int, Recipe> recipeData;
+    public Dictionary<int, Stage> stageData;
 
     // 선택된 레시피와 재료들을 담는 리스트
     private List<Recipe> stageRecipe;
-    //private stagenum_e stagenum;
+
+    // 선택된 스테이지 점수 담는 리스트
+    private List<Stage> stageScore;
 
 
     private void Recipe_DataManager()
     {
-        //stagenum = stagenum_e.sushi1;
+        //레시피
         LoadRecipeFromJson();
         StageRecipeData(1); //스테이지 + 레시피 + 재료 데이터 부르는 메서드
-        //GetStageRecipe();
+        
     }
 
     public static DataManager Instance
@@ -67,39 +70,103 @@ public class DataManager : MonoBehaviour
     
     private void LoadRecipeFromJson()
     {
-        string jsonFile = Resources.Load<TextAsset>("Data/Recipe_JDB").text;
-        var recipes = JsonConvert.DeserializeObject<Recipe[]>(jsonFile);
+        string recipe_jsonFile = Resources.Load<TextAsset>("Data/Recipe_JDB").text;
+        var recipes = JsonConvert.DeserializeObject<Recipe[]>(recipe_jsonFile);
 
 
         // recipes 배열을 Dictionary로 변환
         recipeData = recipes.ToDictionary(x => x.id, x => x);
-
         //Debug.Log("Loaded " + recipeData.Count + " recipes from JSON.");
-
     }
 
-    public void StageRecipeData(int stageNumber)
+    private void LoadStageFromJson()
     {
-        stageRecipe = new List<Recipe>();
-        //stage1Ingredient = new List<string>();
+        string score_jsonFile = Resources.Load<TextAsset>("Data/Stage_JDB").text;
+        var stages = JsonConvert.DeserializeObject<Stage[]>(score_jsonFile);
 
-        // 선택된 레시피 출력 및 재료 출력
-        var stageRecipes = recipeData.Values.Where(recipe => recipe.stage == stageNumber).ToList();
-        //Recipe selectedRecipe = stageRecipes[Random.Range(0, stageRecipes.Count)];
-        Debug.Log(stageRecipes.Count);
-        //foreach (var recipe in stageRecipes)
-        //{
-        //    stageRecipe.Add(recipe);//레시피 추가
-        //    //string ingredient = string.Join(",", recipe.ingredient);
-        //    //stage1Ingredient.Add(ingredient);//해당 레시피의 재료 추가
-        //    //Debug.Log("Selected recipe: " + recipe.recipe);
-        //    //Debug.Log("Ingredients for " + recipe.recipe + ": " + ingredient);
-        //}
+        stageData = stages.ToDictionary(x => x.stage, x => x);
+    }
 
-        //Debug.Log("Selected recipe: " + selectedRecipe.recipe);
-        // 재료들을 문자열로 조합하여 출력
-        //string ingredients = string.Join(", ", selectedRecipe.ingredient);
-        //Debug.Log("Ingredients for " + selectedRecipe.recipe + ": " + ingredients);
+    public List<List<Recipe>> StageRecipeData(int stageNumber)
+    {
+        List<Recipe> stageRecipes = recipeData.Values.Where(recipe => recipe.stage == stageNumber).ToList();
+
+        List<List<Recipe>> allRecipes = new List<List<Recipe>>(); // 모든 레시피를 담을 리스트 생성
+        List<List<string>> allIngredients = new List<List<string>>(); // 모든 재료를 담을 리스트 생성
+
+        foreach (var recipe in stageRecipes)
+        {
+            // 각 레시피를 담는 리스트에 추가
+            allRecipes.Add(new List<Recipe> { recipe });
+
+            List<string> ingredients = new List<string>();
+            foreach (var ingredient in recipe.ingredient)
+            {
+                ingredients.Add(ingredient); // 각 레시피의 재료를 재료 리스트에 추가
+            }
+            allIngredients.Add(ingredients); // 각 레시피의 재료 리스트를 전체 재료 리스트에 추가
+        }
+
+        // 각 리스트들을 출력하여 확인
+        foreach (var recipes in allRecipes)
+        {
+            foreach (var recipe in recipes)
+            {
+                Debug.Log("Recipe: " + recipe.recipe);
+            }
+        }
+
+        foreach (var ingredients in allIngredients)
+        {
+            foreach (var ingredient in ingredients)
+            {
+                Debug.Log("Ingredient: " + ingredient);
+            }
+        }
+
+        // allRecipes를 반환하여 호출한 곳에서 필요한 데이터를 사용
+        return allRecipes;
+    }
+    //public List<Recipe> StageRecipeData(int stageNumber)
+    //{                       
+    //    // 선택된 레시피 출력 및 재료 출력
+    //    List<Recipe> stageRecipes = recipeData.Values.Where(recipe => recipe.stage == stageNumber).ToList();
+
+
+    //    foreach (var recipe in stageRecipes)
+    //    {
+    //        foreach (var inredient in recipe.ingredient)
+    //        { 
+
+    //        }
+    //    }
+
+    //    return stageRecipes;
+
+
+
+    //    //Recipe selectedRecipe = stageRecipes[Random.Range(0, stageRecipes.Count)];
+    //    //Debug.Log(stageRecipes.Count);
+    //    //foreach (var recipe in stageRecipes)
+    //    //{
+    //    //    stageRecipe.Add(recipe);//레시피 추가
+    //    //    //string ingredient = string.Join(",", recipe.ingredient);
+    //    //    //stage1Ingredient.Add(ingredient);//해당 레시피의 재료 추가
+    //    //    //Debug.Log("Selected recipe: " + recipe.recipe);
+    //    //    //Debug.Log("Ingredients for " + recipe.recipe + ": " + ingredient);
+    //    //}
+
+    //    //Debug.Log("Selected recipe: " + selectedRecipe.recipe);
+    //    // 재료들을 문자열로 조합하여 출력
+    //    //string ingredients = string.Join(", ", selectedRecipe.ingredient);
+    //    //Debug.Log("Ingredients for " + selectedRecipe.recipe + ": " + ingredients);
+    //}
+
+    public void StageScoreData(int stageNumber)
+    {
+        stageScore = new List<Stage>();
+
+        var stageScores = stageData.Values.Where(stage => stage.stage == stageNumber).ToList();
 
 
     }
--- Assets/3.Script/Json/Stage.cs
+++ Assets/3.Script/Json/Stage.cs
@@ -6,17 +6,11 @@ using UnityEngine;
 
 public class Stage : MonoBehaviour
 {
-    public enum ingredient_e
-    {
-        Fish,
-        Prawn,
-        Rrice,
-        Seaweed
-    }
 
-    //public int id;
+
+    
     public int stage;
-    public string[] recipe; 
-    //public string[] ingredient;
+    public int[] targetScore; 
+    public int bestScore;
 
 }
