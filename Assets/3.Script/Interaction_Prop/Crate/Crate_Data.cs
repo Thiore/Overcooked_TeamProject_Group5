cf74106633d8f338caa657fea2ac8f9c80f98615
최영훈 <thiore94@gmail.com>
2024-07-17 19:19:59 +0900

    되살리기

---
+++ Assets/3.Script/Interaction_Prop/Crate/Crate_Data.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Crate_Info
+{
+    public eIngredients Ingredients;
+    public Vector3 Position;
+    public Quaternion Rotation;
+}
+
+[CreateAssetMenu(fileName = "_Crate", menuName = "ScriptableObjects/Crate_Data", order = 1)]
+public class Crate_Data : ScriptableObject
+{
+    public Crate_Info[] Install_Pos;
+
+
+}
