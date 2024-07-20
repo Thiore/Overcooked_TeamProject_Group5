d7a0e813792d97398ad98e46e1fd7768e7c7e8a8
최정문 <>
2024-07-20 13:04:42 +0900

    11

--- Assets/3.Script/ETC/TrashCanController.cs
+++ Assets/3.Script/ETC/TrashCanController.cs
@@ -2,10 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrashCanController : MonoBehaviour
+public class TrashCanController : CounterController
 {
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Ingredeint"))
+        {
+            if(this.IsPutOn == true || this.transform.GetChild(0).CompareTag("Ingredeint"))
+            {
 
+            }
+        }
+    }
+
+
+    private IEnumerator DropTrash_co()
+    {
+        float totaltime = 5f;
+        float elapsedtime = 0f;
+
+        while (elapsedtime < totaltime)
+        {
+
+
+            yield return null;
+        }
+
+    }
 
 
 
