3eb169274587102f159e3e43a94b7998b1a935ba
백승주 <tmdwn3431@naver.com>
2024-08-05 13:08:12 +0900

    [Add] URP / Depth Texture

--- Assets/3.Script/Interaction_Prop/Pot/Pot.cs
+++ Assets/3.Script/Interaction_Prop/Pot/Pot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pot : Cookingtool
+public class Pot : MonoBehaviour
 {
     [SerializeField] private Renderer[] renderer;
     [SerializeField] private Texture2D FalloffTexture;
@@ -120,18 +120,18 @@ public class Pot : Cookingtool
 
 
 
-    public override bool CookedCheck(GameObject obj)
-    {
-        if (obj.TryGetComponent(out Ingredient ingre))
-        {
-            Debug.Log(this.name + " 재료확인");
-        }
-        return false;
-    }
+    //public override bool CookedCheck(GameObject obj)
+    //{
+    //    if (obj.TryGetComponent(out Ingredient ingre))
+    //    {
+    //        Debug.Log(this.name + " 재료확인");
+    //    }
+    //    return false;
+    //}
 
-    public override void StartCook()
-    {
-        base.StartCook();
-    }
+    //public override void StartCook()
+    //{
+    //    base.StartCook();
+    //}
 
 }
